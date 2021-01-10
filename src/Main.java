a850e47bb49dadde11cfc20a7fd11a5181c63ba4
Nathan <nathan.everett@students.plymouth.ac.uk>
2021-01-10 01:32:01 +0000

    moved main java to  folder where other classes are

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        //fetch from DB
+        modelStaff model = retrieveStaffFromDatebase();
+        //--
+
+        //create a view: write details down
+        viewStaff view = new viewStaff();
+        controllerStaff controller = new controllerStaff(model, view);
+        controller.updateView();
+        //--
+
+        //update model data
+        controller.setStaffName("Nathan");
+        controller.updateView();
+        //--
+    }
+
+    private static modelStaff retrieveStaffFromDatebase(){
+        modelStaff staff = new modelStaff();
+        staff.setName("Nathan");
+
+        return staff;
+
+    }
+}
